51387ff77479040043eb358166d902da9d0a74c6
qazxsw1240 <qazxsw010528@gmail.com>
2023-10-22 20:38:23 +0900

    add BookmarkInfo test element

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import BookmarkInfo, { BookmarkInfoModalVisibleContext } from "./BookmarkInfo";
+
+import { BookmarkStorage } from "./db/localStorage";
+
 import "./index.css";
 
+const BookmarkInfoModalVisibleContextProvider = BookmarkInfoModalVisibleContext.Provider;
+
 function App() {
+  useEffect(() => {
+    BookmarkStorage.initializeData();
+    BookmarkStorage.updateBookmark("https://youtu.be/5bId3N7QZec", () => { });
+  }, []);
+  const bookmarkInfoModalVisibleContext = useState(false);
+  const [visible, setVisible] = bookmarkInfoModalVisibleContext;
+  const bookmarks = BookmarkStorage.getAllBookmarks();
   return (
-    <div>
+    <>
       <div className="text-window modifiable">
         <div className="text-title text-window-inner">Hello, World!</div>
       </div>
-    </div>
+      <BookmarkInfoModalVisibleContextProvider value={bookmarkInfoModalVisibleContext}>
+        {
+          visible ?
+            <BookmarkInfo bookmark={bookmarks[0]} /> :
+            <></>
+        }
+      </BookmarkInfoModalVisibleContextProvider>
+      <button onClick={() => setVisible(() => true)}>show</button>
+    </>
   );
 }
 
