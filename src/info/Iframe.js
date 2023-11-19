d06918f01ccbb92875924bf50bc441e9debdba85
qazxsw1240 <qazxsw010528@gmail.com>
2023-11-20 03:21:04 +0900

    add jsdoc

--- src/info/Iframe.js
+++ src/info/Iframe.js
@@ -1,16 +1,27 @@
 import React from "react";
 
+/**
+ * @typedef {object} IframeProps
+ * @property {string} title
+ * @property {string} src
+ */
+
+/**
+ * @param {IframeProps&React.HTMLAttributes} props 
+ * @returns {React.JSX.Element}
+ */
 function Iframe(props) {
+  const { style, title, src } = props;
   return (
-    <div style={props.style}>
+    <div style={style}>
       <iframe style={{
         width: "100%",
         height: 360,
         borderWidth: 0,
         borderRadius: 12
       }}
-        title={props.title}
-        src={props.src}
+        title={title}
+        src={src}
         allowFullScreen={true} />
     </div>
   );
