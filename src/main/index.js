3ad19fb5b224306f675a7430579c49bdd00bb298
1871134son <adsfg4295@gmail.com>
2023-11-20 18:17:19 +0900

    add components

---
+++ src/main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Mindex.css';
+import App from './MainP';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
