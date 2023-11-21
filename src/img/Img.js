905d911eee425d86c6c3b87da15a324109457346
qazxsw1240 <qazxsw010528@gmail.com>
2023-11-21 09:53:18 +0900

    refactor image component code

--- src/img/Img.js
+++ src/img/Img.js
@@ -1,15 +1,24 @@
 import React from "react";
 
+/**
+ * @typedef {object} ImgProps
+ * @property {string} src
+ */
+
+/**
+ * @param {ImgProps} props 
+ * @returns {React.JSX.Element}
+ */
 function Img(props) {
+  const { src } = props;
   return (
-    <img style={
-      {
+    <img
+      style={{
         width: "var(--bookmark-image-width)",
         height: "36px",
         float: "left"
-      }
-    }
-      src={props.src}
+      }}
+      src={src}
       alt="thumbnail" />
   );
 }
