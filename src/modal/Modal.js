a925b3e8a5dd11f78ea4bc1caab2ae2d302df17b
qazxsw1240 <qazxsw010528@gmail.com>
2023-11-21 09:52:00 +0900

    refactor modal code

--- src/modal/Modal.js
+++ src/modal/Modal.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./modal.css";
+
 /**
  * @typedef {object} ModalProp
  * @property {boolean} visible
@@ -11,13 +13,14 @@ import React from "react";
  */
 
 function Modal(prop) {
-  const { visible = false, children } = prop;
+  const { visible = false, style, children } = prop;
   return (
-    <div className="modal-background"
+    <div
+      className="modal-background"
       style={{
         display: visible ? "flex" : "none"
       }}>
-      <div className="modal window" style={prop.style}>
+      <div className="modal window" style={style}>
         <div className="window-inner">{children}</div>
       </div>
     </div>
